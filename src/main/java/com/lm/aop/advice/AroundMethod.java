2b105083d3cb9db6b3a6157bcee0fd49552b6a6f
lm <git@lm.com>
2017-12-14 14:55:06 +0800

    123

--- src/main/java/com/lm/aop/advice/AroundMethod.java
+++ src/main/java/com/lm/aop/advice/AroundMethod.java
@@ -1,12 +1,10 @@
 package com.lm.aop.advice;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 //import org.springframework.cglib.proxy.MethodInterceptor;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.cglib.proxy.MethodProxy;
 
 /** 
  * 项目名称：example
@@ -18,14 +16,14 @@ import org.springframework.cglib.proxy.MethodProxy;
  */
 public class AroundMethod implements MethodInterceptor{
 //	@Override
-	public Object intercept(Object arg0, Method method, Object[] arg2, MethodProxy arg3) throws Throwable {
-		System.out.println("Method name : " + method.getName());
-		System.out.println("Method arguments : " + Arrays.toString(arg2));
-		System.out.println("Before method!");
-		Object result = arg3.invokeSuper(arg0, arg2);
-		System.out.println("After method!");
-		return result;
-	}
+//	public Object intercept(Object arg0, Method method, Object[] arg2, MethodProxy arg3) throws Throwable {
+//		System.out.println("Method name : " + method.getName());
+//		System.out.println("Method arguments : " + Arrays.toString(arg2));
+//		System.out.println("Before method!");
+//		Object result = arg3.invokeSuper(arg0, arg2);
+//		System.out.println("After method!");
+//		return result;
+//	}
 
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
